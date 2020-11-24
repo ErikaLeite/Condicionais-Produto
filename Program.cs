cb6fdb1fba172808e9003729fdf0db9838757eb4
ERIKA LEITE PAIVA <you@example.com>
2020-11-24 14:19:35 -0300

    Exercicio 99% consluído, corrigir erro de 5% de desc

--- Program.cs
+++ Program.cs
@@ -8,32 +8,45 @@ namespace Condicionais_Produto
         {   
             string nome = "Sabão";
             Console.WriteLine("Escolha a quantidade de "+nome+" e ganhe descontos!\n");
-            Console.WriteLine("Digite a quantodade escolhida: ");
+            Console.WriteLine("Nas compras de até 5 unidades ganhe 2% de desconto\nNas compras de até 10 unidades ganhe 3% de desconto\nNas compras acima de 10 unidades ganhe 5% de desconto\n");
+            Console.WriteLine("Digite a quantidade escolhida: ");
             double quant = double.Parse(Console.ReadLine());
 
+            //VARIAVEIS P/ CALCULO DE DESCONTO
             double unit = 10;
-            double final = quant * unit;
-            
+            double desc2P= 0.02d;
+            double desc3P= 0.03d;
+            double desc5P= 0.05d; //valores dessa desc imprimindo apenas 3% ao invés de 5. *Corrigir*
 
-            Console.WriteLine("O valor de "+nome+" é R$ "+unit+ ",00 \n");
+            Console.WriteLine("O valor final de sua compra é de: R$ "+CalculoDescontos(quant, unit, desc2P, desc3P, desc5P));
 
-            if(quant<=5){
 
-                double perc2= (2/100)*unit;
-                double desc2 = final - perc2;
 
-                Console.WriteLine("O valor total: R$"+final+ ",00\nCom o desconto aplicado, sua compra será de: R$ "+desc2);
+            //FUNÇÃO DESCONTO
+            double CalculoDescontos(double quant, double unit, double desc1, double desc2, double desc3)
+            {
+                if(quant <=5)
+                {
+                    double valorFinal = quant * unit;
+                    double desconto = (quant*unit)*desc1;
+
+                    return valorFinal - desconto;
+                }
+                else if (quant >5 |quant <=10)
+                {
+                    double valorFinal = quant * unit;
+                    double desconto = (quant*unit)*desc2;
+                    return valorFinal - desconto;
+                }
+                else if(quant >11)
+                {   double valorFinal = quant * unit;
+                    double desconto = (quant*unit)*desc3;
+                    return valorFinal - desconto;
+                }else{
+                    return 0;
+                }
+
             }
-            // else if(quant >5){
-
-            //     Console.WriteLine("O valor total: R$"+final+ ",00\nCom o desconto aplicado, sua compra será de: R$ "+desc2);
-
-            // }else(quant >10){
-                
-            //     Console.WriteLine("O valor total: R$"+final+ ",00\nCom o desconto aplicado, sua compra será de: R$ "+desc2);
-
-            // }
-
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Condicionais-Produto
 Faça um algoritmo para ler: a descrição do produto (nome), a quantidade adquirida e o preço unitário. Calcular e escrever o total (total = quantidade adquirida * preço unitário), o desconto e o total a pagar (total a pagar = total - desconto),
+
+
+<!-- Se quantidade &lt;= 5 o desconto será de 2%
+- Se quantidade &gt; 5 e quantidade &lt;=10 o desconto será de 3%
+- Se quantidade &gt; 10 o desconto será de 5%
+Dica: utilize if / else if / else -->
